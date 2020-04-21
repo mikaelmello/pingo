00f2c1ddc19c77ac116d5e27ef399082f6fe0f48
Mikael Mello <git@mikaelmello.com>
2020-04-21 00:16:39 -0300

    Add new properties to bundle

--- core/bundle.go
+++ core/bundle.go
@@ -1,6 +1,33 @@
 package core
 
+import (
+	"net"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
+)
+
 // Bundle is an aggregation of ping executions
 type Bundle struct {
-	target string
+	id int
+
+	bigID int64
+
+	currentSequence int
+
+	totalSent int
+
+	address net.Addr
+
+	isIPv4 bool
+}
+
+// GetICMPType returns the appropriate type to be used in the ICMP request of this bundle
+func (b *Bundle) GetICMPType() icmp.Type {
+	if b.isIPv4 {
+		return ipv4.ICMPTypeEcho
+	}
+
+	return ipv6.ICMPTypeEchoRequest
 }
