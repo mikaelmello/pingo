105c932f2076df5bbf4f3af1b2369bb5d6bf36e0
Mikael Mello <git@mikaelmello.com>
2020-04-21 00:22:42 -0300

    Add more utils

--- core/utils.go
+++ core/utils.go
@@ -1,6 +1,10 @@
 package core
 
-import "net"
+import (
+	"encoding/binary"
+	"net"
+	"time"
+)
 
 func isIPv4(ip net.IP) bool {
 	return ip.To4() != nil
@@ -8,4 +12,23 @@ func isIPv4(ip net.IP) bool {
 
 func isIPv6(ip net.IP) bool {
 	return len(ip) == net.IPv6len
+}
+
+func unixNanoToBytes(t time.Time) []byte {
+	return int64ToBytes(t.UnixNano())
+}
+
+func bytesToUnixNano(b []byte) time.Time {
+	unixNano := bytesToInt64(b)
+	return time.Unix(0, unixNano)
+}
+
+func int64ToBytes(i int64) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(i))
+	return b
+}
+
+func bytesToInt64(b []byte) int64 {
+	return int64(binary.BigEndian.Uint64(b))
 }
