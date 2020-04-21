979678e9a71cd26ef5ba2e3cd60bb1ba190aefa9
Mikael Mello <git@mikaelmello.com>
2020-04-21 03:46:39 -0300

    Add flow of bundle

--- core/bundle.go
+++ core/bundle.go
@@ -4,22 +4,29 @@ import (
 	"math"
 	"math/rand"
 	"net"
+	"sync"
 	"time"
 )
 
 // Bundle is an aggregation of ping executions
 type Bundle struct {
-	id int
+	ttl            int
+	count          int
+	interval       time.Duration
+	timeout        time.Duration
+	deadline       time.Duration
+	deadlineActive bool
 
-	bigID uint64
-
+	id              int
+	bigID           uint64
 	currentSequence int
-
-	totalSent int
-
-	address net.Addr
-
-	isIPv4 bool
+	totalSent       int
+	totalReceived   int
+	maxRtt          int64
+	rtts            []int64
+	address         net.Addr
+	isIPv4          bool
+	finished        chan bool
 }
 
 // NewBundle creates a new Bundle
@@ -33,11 +40,77 @@ func NewBundle(addr string) (*Bundle, error) {
 
 	r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 	return &Bundle{
+		ttl:            64,
+		count:          -1,
+		interval:       time.Second * 1,
+		timeout:        time.Second * 10,
+		deadline:       -1,
+		deadlineActive: false,
+
 		id:              r.Intn(math.MaxUint16),
 		bigID:           r.Uint64(),
 		currentSequence: 0,
 		totalSent:       0,
+		totalReceived:   0,
+		maxRtt:          0,
 		address:         ipaddr,
 		isIPv4:          ipv4,
 	}, nil
+}
+
+// Start starts the sequence of pings
+func (b *Bundle) Start() error {
+	conn, err := b.GetConnection()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	deadline := time.NewTimer(b.deadline)
+	defer deadline.Stop()
+
+	timeout := time.NewTimer(b.timeout)
+	defer timeout.Stop()
+
+	interval := time.NewTimer(b.interval)
+	defer interval.Stop()
+
+	rawPackets := make(chan *rawPacket, 5)
+	defer close(rawPackets)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go b.pollICMP(&wg, conn, rawPackets)
+
+	for {
+		select {
+		case <-deadline.C:
+			if !b.deadlineActive {
+				continue
+			}
+			return nil
+		case <-timeout.C:
+			interval.Reset(0)
+			continue
+		case <-interval.C:
+			err = b.requestEcho(conn)
+			if err != nil {
+				interval.Reset(b.interval)
+				continue
+			}
+
+			// get max rtt * 2
+			duration := time.Duration(2 * b.maxRtt)
+			timeout.Reset(duration)
+		case raw := <-rawPackets:
+			match, err := b.checkRawPacket(raw)
+
+			if err != nil || !match {
+				continue
+			}
+
+			timeout.Stop()
+			interval.Reset(b.interval)
+		}
+	}
 }
