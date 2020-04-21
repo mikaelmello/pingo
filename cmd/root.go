a643d068353113e7d9a7b16a83a130fadb74fbf1
Mikael Mello <git@mikaelmello.com>
2020-04-21 03:46:47 -0300

    Run bundle start

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/mikaelmello/pingo/core"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +13,18 @@ var rootCmd = &cobra.Command{
 	Long:  "pingo is a Go implementation of the ping utility",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello, World!")
+		bundle, err := core.NewBundle(args[0])
+		if err != nil {
+			fmt.Printf("Error %s", err.Error())
+			return
+		}
+
+		err = bundle.Start()
+		if err != nil {
+			fmt.Printf("Error %s", err.Error())
+			return
+		}
+
 	},
 }
 
