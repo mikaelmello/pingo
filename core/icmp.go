2e4b0049fda37189fab0d9184713c52e2f00e86d
Mikael Mello <git@mikaelmello.com>
2020-04-21 03:46:27 -0300

    Add functions to receive icmp

--- core/icmp.go
+++ core/icmp.go
@@ -3,7 +3,7 @@ package core
 import (
 	"fmt"
 	"net"
-	"syscall"
+	"sync"
 	"time"
 
 	"golang.org/x/net/icmp"
@@ -15,6 +15,7 @@ const (
 	echoCode       = 0
 	icmpProtocol   = 1
 	icmpv6Protocol = 58
+	dataLength     = 16
 	icmpNetwork    = "ip4:icmp"
 	icmpv6Network  = "ip6:ipv6-icmp"
 )
@@ -43,26 +44,117 @@ func (b *Bundle) requestEcho(conn *icmp.PacketConn) error {
 		return err
 	}
 
-	for {
-		_, err := conn.WriteTo(msgBytes, b.address)
-
-		if err == nil {
-			break
-		}
-
-		neterr, ok := err.(*net.OpError)
-
-		if ok && neterr.Err == syscall.ENOBUFS {
-			continue
-		}
-
-		break
-	}
-
+	_, err = conn.WriteTo(msgBytes, b.address)
 	b.totalSent++
 	b.currentSequence++
 
-	return nil
+	return err
+}
+
+func (b *Bundle) pollICMP(
+	wg *sync.WaitGroup,
+	conn *icmp.PacketConn,
+	recv chan<- *rawPacket,
+) {
+	defer wg.Done()
+	for {
+		select {
+		case <-b.finished:
+			return
+		default:
+			buffer := make([]byte, 1024)
+			conn.SetReadDeadline(time.Now().Add(time.Second * 1))
+			length, ttl, err := b.readFrom(conn, buffer)
+			if err != nil {
+				if neterr, ok := err.(*net.OpError); ok {
+					if neterr.Timeout() {
+						continue
+					} else {
+						close(b.finished)
+						return
+					}
+				}
+			}
+
+			recv <- &rawPacket{content: buffer, length: length, ttl: ttl}
+		}
+	}
+}
+
+func (b *Bundle) readFrom(conn *icmp.PacketConn, bytes []byte) (int, int, error) {
+	var length int
+	var ttl int
+	var err error
+	if b.isIPv4 {
+		var cm *ipv4.ControlMessage
+		length, cm, _, err = conn.IPv4PacketConn().ReadFrom(bytes)
+		if cm != nil {
+			ttl = cm.TTL
+		}
+	} else {
+		var cm *ipv6.ControlMessage
+		length, cm, _, err = conn.IPv6PacketConn().ReadFrom(bytes)
+		if cm != nil {
+			ttl = cm.HopLimit
+		}
+	}
+
+	return length, ttl, err
+}
+
+func (b *Bundle) checkRawPacket(raw *rawPacket) (bool, error) {
+	receivedTstp := time.Now()
+
+	m, err := icmp.ParseMessage(b.GetProtocol(), raw.content)
+	if err != nil {
+		return false, fmt.Errorf("Error parsing ICMP message: %s", err.Error())
+	}
+
+	if m.Code != echoCode || (m.Type != ipv4.ICMPTypeEchoReply && m.Type != ipv6.ICMPTypeEchoReply) {
+		// Not an echo reply, ignore it
+		return false, nil
+	}
+
+	switch body := m.Body.(type) {
+	case *icmp.Echo:
+		// // If we are privileged, we can match icmp.ID
+		// if p.network == "ip" {
+		// 	// Check if reply from same ID
+		// 	if pkt.ID != p.id {
+		// 		return nil
+		// 	}
+		// }
+
+		if len(body.Data) < dataLength {
+			return false, fmt.Errorf("Missing data, %d bytes out of %d", len(body.Data), dataLength)
+		}
+
+		bigID := bytesToUint64(body.Data[8:])
+		tstp := bytesToUnixNano(body.Data[:8])
+
+		if body.ID != b.id {
+			return false, nil
+		}
+
+		if body.Seq != b.currentSequence {
+			return false, nil
+		}
+
+		if bigID != b.bigID {
+			return false, nil
+		}
+
+		rtt := receivedTstp.Sub(tstp).Nanoseconds()
+		if rtt > b.maxRtt {
+			b.maxRtt = rtt
+		}
+		println(rtt)
+		b.rtts = append(b.rtts, rtt)
+		b.totalReceived++
+		return true, nil
+	default:
+		return false, fmt.Errorf("Invalid body type: '%T'", body)
+	}
 }
 
 // GetICMPType returns the appropriate type to be used in the ICMP request of this bundle
